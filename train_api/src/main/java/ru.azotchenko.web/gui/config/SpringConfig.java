4bbe5e922c052e387e8e84c034dd814231d5dec2
AnyaZ03 <azotchenko03@mail.ru>
2025-03-30 10:07:17 +0300

    ДЗ 7.1 и 8.1

--- train_api/src/main/java/ru.azotchenko.web/gui/config/SpringConfig.java
+++ train_api/src/main/java/ru.azotchenko.web/gui/config/SpringConfig.java
@@ -1,15 +1,14 @@
-package ru.azotchenko.gui.config;
+package ru.azotchenko.web.gui.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import ru.azotchenko.spring.hibernate.config.DbConfig;
-
 @Configuration
-@PropertySource("jdbc.properties")
 @Import(DbConfig.class)
 @ComponentScan(basePackages = "ru.azotchenko")
+@PropertySource("jdbc.properties")
 public class SpringConfig {
 
 }
